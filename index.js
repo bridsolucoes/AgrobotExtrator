37818c578b47e715e7be74790671b537f4c3df45
feLiruc <fim1991@hotmail.com>
2020-03-05 14:15:11 -0300

    Updating to correctly get the correct content from the COTACOES.

--- index.js
+++ index.js
@@ -25,56 +25,56 @@ if(dev){
 
 		/// Dólar
 		await page.goto('https://www.canalrural.com.br/cotacao/mercado_financeiro/', {waitUntil: 'networkidle2'});
-		await page.setViewport({width: 800, height: 630});
-		await page.$eval('div.algodao > div > div:nth-child(3) > h3', (el) => el.scrollIntoView());
-		await page.screenshot({
-			path: folder + '00 - dolar.jpg',
-			type: 'jpeg',
-			quality: 70
+		await page.evaluate(() => document.getElementById("abas").remove());
+		var el = await page.$('.algodao');
+		await el.screenshot({
+		    path: folder + '00 - dolar.jpg',
+		    type: 'jpeg',
+		    quality: 70
 		});
 		/// Fim Dólar
 
 		/// Milho
 		await page.goto('https://www.canalrural.com.br/cotacao/milho/', {waitUntil: 'networkidle2'});
-		await page.setViewport({width: 800, height: 510});
-		await page.$eval('div.milho > div > div.sub-title > h3', (el) => el.scrollIntoView());
-		await page.screenshot({
-			path: folder + '01 - milho.jpg',
-			type: 'jpeg',
-			quality: 70
+		await page.evaluate(() => document.getElementById("abas").remove());
+		var el = await page.$('.milho');
+		await el.screenshot({
+		    path: folder + '01 - milho.jpg',
+		    type: 'jpeg',
+		    quality: 70
 		});
 		/// Fim Milho
 
 		/// Soja
 		await page.goto('https://www.canalrural.com.br/cotacao/soja/', {waitUntil: 'networkidle2'});
-		await page.setViewport({width: 800, height: 1640});
-		await page.$eval('div.trigo > div > div:nth-child(3) > h3', (el) => el.scrollIntoView());
-		await page.screenshot({
-			path: folder + '02 - soja.jpg',
-			type: 'jpeg',
-			quality: 70
+		await page.evaluate(() => document.getElementById("abas").remove());
+		var el = await page.$('.trigo');
+		await el.screenshot({
+		    path: folder + '02 - soja.jpg',
+		    type: 'jpeg',
+		    quality: 70
 		});
 		/// Fim Soja
 
 		/// Café
 		await page.goto('https://www.canalrural.com.br/cotacao/cafe/', {waitUntil: 'networkidle2'});
-		await page.setViewport({width: 800, height: 1200});
-		await page.$eval('div.trigo > div > div:nth-child(3) > h3', (el) => el.scrollIntoView());
-		await page.screenshot({
-			path: folder + '03 - cafe.jpg',
-			type: 'jpeg',
-			quality: 70
+		await page.evaluate(() => document.getElementById("abas").remove());
+		var el = await page.$('.trigo');
+		await el.screenshot({
+		    path: folder + '03 - cafe.jpg',
+		    type: 'jpeg',
+		    quality: 70
 		});
 		/// Fim Café
 
 		/// Boi Gordo
 		await page.goto('https://www.canalrural.com.br/cotacao/boi-gordo/', {waitUntil: 'networkidle2'});
-		await page.setViewport({width: 800, height: 1300});
-		await page.$eval('div.boi-gordo > div > div.sub-title > h3', (el) => el.scrollIntoView());
-		await page.screenshot({
-			path: folder + '04 - boigordo.jpg',
-			type: 'jpeg',
-			quality: 70
+		await page.evaluate(() => document.getElementById("abas").remove());
+		var el = await page.$('.boi-gordo');
+		await el.screenshot({
+		    path: folder + '04 - boigordo.jpg',
+		    type: 'jpeg',
+		    quality: 70
 		});
 		/// Fim Boi Gordo
 
